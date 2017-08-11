5c2b8fb1cb620762fdbb92b23de09bbaa9414485
fadawar <fadawar@gmail.com>
2017-08-11 21:53:25 +0200

    #6 - Add Bednar, Breweria, Dream's, Gastrohouse, other restaurants

--- infinit_lunch.py
+++ infinit_lunch.py
@@ -1,83 +1,18 @@
 import os
-import json
-import re
 from datetime import datetime
-import requests
-from bs4 import BeautifulSoup
-from flask import Flask
-import time
-import restaurants
 
-# SLACK_HOOK = 'https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX'
+import requests
+from flask import Flask
+
+import restaurants
 from slack import Channel
 
+# SLACK_HOOK = 'https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX'
 SLACK_HOOK = os.environ.get('SLACK_HOOK', None)
 SECRET_KEY = os.environ.get('SECRET_KEY', None)
-FB_APP_ID = os.environ.get('FB_APP_ID', None)
-FB_APP_SECRET = os.environ.get('FB_APP_SECRET', None)
 DEBUG = bool(os.environ.get('DEBUG', False))
 
 app = Flask(__name__)
-
-
-def check_for_errors(func):
-    def func_wrapper(url):
-        try:
-            r = requests.get(url)
-            if r.ok:
-                soup = BeautifulSoup(r.content, 'html.parser')
-                day = datetime.today().weekday()
-                return func(soup, day)
-        except IndexError:
-            return ['Scrapping problem. Fix it: https://github.com/fadawar/infinit-lunch']
-        except Exception:
-            return ['Unknown error']
-        return ['Problem with request']
-    return func_wrapper
-
-
-@check_for_errors
-def scrap_dreams(soup, day):
-    elements = soup.find_all('td', id='jedlo')
-    return [el.text for el in elements]
-
-
-@check_for_errors
-def scrap_breweria(soup, day):
-    elements = soup.select('.tabs__pane')[day].select('.desc__content')
-    return [el.text for el in elements if len(el.text) > 1]
-
-
-@check_for_errors
-def scrap_bednar(soup, day):
-    groups = re.search(r'PONDELOK(.*)UTOROK(.*)STREDA(.*)ŠTVRTOK(.*)PIATOK(.*)BEDNAR', soup.text, re.DOTALL).groups()
-    return [i for i in groups[day].split('\n') if i]
-
-
-@check_for_errors
-def scrap_gastrohouse(soup, day):
-    els = soup.select('.td-main-page-wrap')[0].select('ul')[-1].select('li')
-    return [i.text for i in els]
-
-
-def get_other_restaurants():
-    return ['Panda :panda_face:\nCigipanda :man::skin-tone-5:\nPunjabi Dhaba :man_with_turban:\nCasa Inka :dancer:\n'
-            'Freshmarket :watermelon:']
-
-
-def send_to_slack(messages, secret_key):
-    if SLACK_HOOK and secret_key == SECRET_KEY:
-        for msg in messages:
-            requests.post(SLACK_HOOK, data=json.dumps({'text': msg}))
-
-
-def create_message(items):
-    messages = ['*MENU {}*\n'.format(datetime.today())]
-    for item in items:
-        msg = '\n\n*{}*\n'.format(item['restaurant'])
-        msg += '\n'.join(item['menu'])
-        messages.append(msg)
-    return messages
 
 
 def is_work_day():
@@ -86,8 +21,13 @@ def is_work_day():
 
 def retrieve_menus():
     return [
+        restaurants.BednarRestaurant().retrieve_menu(),
+        restaurants.BreweriaRestaurant().retrieve_menu(),
         restaurants.DonQuijoteRestaurant().retrieve_menu(),
+        restaurants.DreamsRestaurant().retrieve_menu(),
+        restaurants.GastrohouseRestaurant().retrieve_menu(),
         restaurants.JarosovaRestaurant().retrieve_menu(),
+        restaurants.OtherRestaurant().retrieve_menu(),
     ]
 
 
@@ -95,24 +35,13 @@ def should_send_to_slack(secret_key):
     return SLACK_HOOK and secret_key == SECRET_KEY
 
 
-@app.route('/', defaults={'secret_key': 'wrong key :('})
+@app.route('/', defaults={'secret_key': 'wrong key'})
 @app.route('/<secret_key>')
 def hello(secret_key):
     if is_work_day():
         menus = restaurants.FormattedMenus(retrieve_menus())
         if should_send_to_slack(secret_key):
             Channel(SLACK_HOOK, requests).send(menus)
-
-        # msg = create_message([
-        #     {'restaurant': 'Dream\'s', 'menu': scrap_dreams('http://www.dreams-res.sk/menu/daily_menu_sk.php')},
-        #     {'restaurant': 'Breweria', 'menu': scrap_breweria('http://breweria.sk/slimak/menu/denne-menu/')},
-        #     {'restaurant': 'Bednar', 'menu': scrap_bednar('http://bednarrestaurant.sk/new/wordpress/?page_id=62')},
-        #     {'restaurant': 'Jedalen Jarosova', 'menu': scrap_jarosova('http://vasestravovanie.sk/jedalny-listok-jar/')},
-        #     {'restaurant': 'Gastrohouse (vyvarovna Slimak)', 'menu': scrap_gastrohouse('http://gastrohouse.sk')},
-        #     {'restaurant': 'Don Quijote', 'menu': scrap_don_quijote()},
-        #     {'restaurant': 'Ine (hlasuj pomocou emoji)', 'menu': get_other_restaurants()},
-        # ])
-        # send_to_slack(msg, secret_key)
         return '<pre>{}</pre>'.format(menus)
     else:
         return 'Come on Monday-Friday'
--- restaurants.py
+++ restaurants.py
@@ -88,6 +88,64 @@ class Restaurant(abc.ABC):
         pass
 
 
+class BednarRestaurant(Restaurant):
+    def __init__(self) -> None:
+        super().__init__()
+        self.content = None
+        self.name = 'Bednar'
+        self.url = 'http://bednarrestaurant.sk/new/wordpress/?page_id=62'
+
+    def retrieve_menu(self, day=TODAY) -> Menu:
+        r = requests.get(self.url)
+        self.content = BeautifulSoup(r.content, 'html.parser')
+        return self.parse_menu(day)
+
+    def parse_menu(self, day):
+        menu = Menu(self.name)
+        try:
+            groups = re.search(r'PONDELOK(.*)UTOROK(.*)STREDA(.*)ŠTVRTOK(.*)PIATOK(.*)BEDNAR',
+                               self.content.text,
+                               re.DOTALL
+                               ).groups()
+            for food in groups[day].split('\n'):
+                if food.strip():
+                    food = re.findall(r'\s*-\s*(.*)', food.strip())[0]
+                    menu.add_item(food)
+        except (IndexError, AttributeError) as ex:
+            menu.add_item('Problem with parsing - {}'.format(ex))
+        return menu
+
+
+class BreweriaRestaurant(Restaurant):
+    def __init__(self) -> None:
+        super().__init__()
+        self.content = None
+        self.name = 'Breweria'
+        self.url = 'http://breweria.sk/slimak/menu/denne-menu/'
+
+    def retrieve_menu(self, day=TODAY) -> Menu:
+        r = requests.get(self.url)
+        self.content = BeautifulSoup(r.content, 'html.parser')
+        return self.parse_menu(day)
+
+    def parse_menu(self, day):
+        menu = Menu(self.name)
+        try:
+            day_elements = self.content.select('.tabs__pane')[day].select('p')
+            menu.add_item(self.clean_food_name(day_elements[0].text))
+            for price, food in zip(day_elements[1::2], day_elements[2::2]):
+                menu.add_item(self.clean_food_name(food.text), self.clean_price(price.text))
+        except IndexError as ex:
+            menu.add_item('Problem with parsing - {}'.format(ex))
+        return menu
+
+    def clean_food_name(self, name):
+        return re.findall(r'\s*[lg]+\.\s+(.*)', name)[0]
+
+    def clean_price(self, price):
+        return float(re.findall(r'(\d,\d{2})', price)[0].replace(',', '.'))
+
+
 class DonQuijoteRestaurant(Restaurant):
     def __init__(self) -> None:
         super().__init__()
@@ -127,6 +185,56 @@ class DonQuijoteRestaurant(Restaurant):
         return re.findall(r'\w\s(.*) \(.*\)\s*', content)
 
 
+class DreamsRestaurant(Restaurant):
+    def __init__(self) -> None:
+        super().__init__()
+        self.content = None
+        self.name = 'Dream\'s'
+        self.url = 'http://www.dreams-res.sk/menu/daily_menu_sk.php'
+
+    def retrieve_menu(self, day=TODAY) -> Menu:
+        r = requests.get(self.url)
+        self.content = BeautifulSoup(r.content, 'html.parser')
+        return self.parse_menu(day)
+
+    def parse_menu(self, day):
+        menu = Menu(self.name)
+        foods = self.content.find_all('td', id='jedlo')
+        prices = self.content.find_all('td', id='cena')
+        for food, price in zip(foods, prices):
+            try:
+                food = re.findall(r'(.*)\s+(?:\S)', food.text)[0]
+                price = price.text.strip().replace(',', '.')
+                if price:
+                    menu.add_item(food, float(price[:-2]))
+                else:
+                    menu.add_item(food)
+            except IndexError as ex:
+                menu.add_item('Problem with parsing - {} - {}'.format(ex, food.text))
+        return menu
+
+
+class GastrohouseRestaurant(Restaurant):
+    def __init__(self) -> None:
+        super().__init__()
+        self.content = None
+        self.name = 'Gastrohouse a.k.a. vývarovňa Slimák (3.8€)'
+        self.url = 'http://gastrohouse.sk/'
+
+    def retrieve_menu(self, day=TODAY) -> Menu:
+        r = requests.get(self.url)
+        self.content = BeautifulSoup(r.content, 'html.parser')
+        return self.parse_menu(day)
+
+    def parse_menu(self, day):
+        menu = Menu(self.name)
+        blocks = self.content.select('.td-main-page-wrap')[0].select('ul')
+        menu_block = blocks[-2] if len(blocks) == 7 else blocks[-1]     # if there is menu for 2 days, there is 7 blocks
+        for food in menu_block.select('li'):                            # if there is menu for 1 day, there is only 6 blocks
+            menu.add_item(food.text)
+        return menu
+
+
 class JarosovaRestaurant(Restaurant):
     def __init__(self) -> None:
         super().__init__()
@@ -150,3 +258,21 @@ class JarosovaRestaurant(Restaurant):
             menu.add_item(i.select('span')[2].text if idx == 1 else i.select('span')[3].text)
         return menu
 
+
+class OtherRestaurant(Restaurant):
+    def __init__(self) -> None:
+        super().__init__()
+        self.content = None
+        self.name = 'Iné (hlasuj pomocou emoji)'
+        self.url = None
+
+    def retrieve_menu(self, day=TODAY) -> Menu:
+        menu = Menu(self.name)
+        menu.add_item('Panda (5.8€) :panda_face:')
+        menu.add_item('Cigipanda :man::skin-tone-5:')
+        menu.add_item('Punjabi Dhaba :man_with_turban:')
+        menu.add_item('Casa Inka :dancer:')
+        menu.add_item('Freshmarket :watermelon:')
+        menu.add_item('Kantína :fork_and_knife:')
+        menu.add_item('Kebab Miletička :taco:')
+        return menu
